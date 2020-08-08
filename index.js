a9bdc5dfd354e2b75488d58c424ed3488b375e54
Thuynguyen729 <57916099+Thuynguyen729@users.noreply.github.com>
2020-08-07 23:49:24 -0700

    To Top Button

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 ScrollReveal({ reset: true });
 ScrollReveal().reveal('.tshirt',{ easing: 'ease-in-out', delay: 300 } );
 ScrollReveal().reveal('.hoodie', { easing: 'ease-in-out', delay: 300 } );
+
+var btn = $('#toTop');
+
+        $(window).scroll(function() {
+        if ($(window).scrollTop() > 1000) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+        });
+
+        btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+        });
